3f77d406c56f39fb0da40352306a095812437c2e
Chengshu Li <chengshuli@rescomp-15-321970.stanford.edu>
2016-01-25 23:26:19 -0800

    add less compilation into gulp

--- codalab/apps/web/gulpfile.js
+++ codalab/apps/web/gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var del = require('del');
+var less = require('gulp-less');
+var path = require('path');
 
 // Compile JSX for production
 gulp.task('default', function() {
@@ -14,4 +16,12 @@ gulp.task('default', function() {
 // Cleanup compiled jsx
 gulp.task('clean', function() {
     del(['static/dist']);
+});
+ 
+gulp.task('less', function () {
+  return gulp.src('static/less/imports.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('static/css'));
 });
