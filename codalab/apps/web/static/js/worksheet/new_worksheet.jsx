7476fabc2f567c0255c75403898e25301b49c390
Chengshu Li <chengshuli@rescomp-15-321970.stanford.edu>
2016-02-29 20:23:59 -0800

    add style

--- codalab/apps/web/static/js/worksheet/new_worksheet.jsx
+++ codalab/apps/web/static/js/worksheet/new_worksheet.jsx
@@ -16,51 +16,28 @@ var NewWorksheet = React.createClass({
     this.setState({showNewWorksheet: !this.state.showNewWorksheet});
   },
 
-  buildRunBundle: function(e) {
-    e.preventDefault();
-    var command = this.createCommand()
-    console.log(command)
-    var response = $('#command_line').terminal().exec(command);
-    console.log(response);
-    this.setState({clCommand: command});
+  handleNameChange: function(event) {
+    this.setState({newWorksheetName: event.target.value});
   },
 
-    handleNameChange: function(event) {
-      this.setState({newWorksheetName: event.target.value});
-    },
-
-    createCommand: function(e) {
-      var command = ['cl run'];
-      for (var i = 0; i < this.state.dependencyKeyList.length; i++) {
-        var key = this.state.dependencyKeyList[i];
-        var target = this.state.selectedDependencies[i];
-        target = target.path === '' ? target.uuid : target.uuid + '/' + target.path
-        command.push(key + ':' + target);
-      }
-      if (this.state.command != null) {
-        command.push('\'' + this.state.command + '\'')
-      }
-      // if (this.state.name != null) {
-      //   command.push('-n')
-      //   command.push(this.state.name)
-      // }
-      command = command.join(' ');
-      return command;
-    // cl run sort.py:sort.py input:a.txt 'python sort.py < input > output' -n sort-run
-    },
+  createNewWorksheet: function(e) {
+    var command = 'cl new ' + this.state.newWorksheetName;
+    $('#command_line').terminal().exec(command);
+    this.setState({newWorksheetName: ''});
+  },
 
   render: function () {
     var new_worksheet_name = (<div>
-        <input type='text' className='' value={this.state.newWorksheetName} onChange={this.props.handleNameChange}></input>
+        <input type='text' className='' value={this.state.newWorksheetName} onChange={this.handleNameChange}></input>
       </div>);
     return (
       <div>
         <div id='new-worksheet'>
-          <span className='close' onClick={this.toggleBuilder}>×</span>
+          <span className='close' onClick={this.toggleNewWorksheet}>×</span>
           New Worksheet Name: {new_worksheet_name}
-          <button id='new-worksheet-button' onClick={this.buildRunBundle}>Run</button>
+          <button className='pop-up-button' onClick={this.createNewWorksheet}>Create</button>
         </div>
-        <button onClick={this.toggleNewWorksheet}>Build Run Bunddle</button>
+        <button onClick={this.toggleNewWorksheet}>Create New Worksheet</button>
       </div>
       );
   }
