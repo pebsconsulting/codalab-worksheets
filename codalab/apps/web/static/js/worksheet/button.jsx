ea24feea865aa8cb09c099cc9bfc07b449c605f3
Chengshu Li <chengshuli@DN526aj4.SUNet>
2016-03-08 13:09:53 -0800

    seprate PR

--- codalab/apps/web/static/js/worksheet/button.jsx
+++
@@ -1,6 +0,0 @@
-var Button = React.createClass({
-  render: function () {
-    var classname = 'button button-' + this.props.type;
-    return (<a className={classname} onClick={this.props.handleClick}>{this.props.text}</a>)
-  }
-});
