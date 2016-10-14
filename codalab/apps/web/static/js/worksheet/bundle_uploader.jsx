046eaf408f44091bfd8b2b21778a5b023f2364cc
Chengshu Li <chengshuli@DN0a22c61d.SUNet>
2016-10-14 16:27:43 -0700

    call new upload bundle rest api on the web and show progress bar

--- codalab/apps/web/static/js/worksheet/bundle_uploader.jsx
+++ codalab/apps/web/static/js/worksheet/bundle_uploader.jsx
@@ -1,4 +1,5 @@
-
+const PROGRESS_BAR_ID = "progressbar-";
+const PROGRESS_LABEL_ID = "progressbar-label-";
 var BundleUploader = React.createClass({
   getInitialState: function() {
     // Maintain a table of the currently uploading bundles.
@@ -21,6 +22,14 @@ var BundleUploader = React.createClass({
     delete newUploading[key];
     this.setState({uploading: newUploading});
   },
+  getQueryParams: function(filename) {
+    var queryParams = {
+      'finalize': 1,
+      'filename': filename,
+      'unpack': pathIsArchive(filename) ? 1 : 0,
+    }
+    return $.param(queryParams);
+  },
   uploadBundle: function (e) {
     e.stopPropagation();
     e.preventDefault();
@@ -31,26 +40,61 @@ var BundleUploader = React.createClass({
       return;
     }
     this.refs.fileDialog.getDOMNode().value = null;
-
-    var fileEntryKey = this.addUploading(file);
-
-    var fd = new FormData();
-    fd.append('file', file);
-    fd.append('bundle_type', 'dataset');
-    fd.append('worksheet_uuid', this.props.ws.info.uuid);
+    var createBundleData = getDefaultBundleMetadata(file.name);
+    var self = this;
     $.ajax({
-      url: '/rest/api/bundles/upload/',
-      data: fd,
-      processData: false,
-      contentType: false,
-      cache: false,
+      url: '/rest/bundles?worksheet=' + this.props.ws.info.uuid,
+      data: JSON.stringify(createBundleData),
+      contentType: 'application/json',
       type: 'POST',
       success: function (data, status, jqXHR) {
-        this.clearUploading(fileEntryKey);
-        this.props.refreshWorksheet();
+        var bundleUuid = data.data[0].id;
+        var fileEntryKey = this.addUploading([file.name, bundleUuid]);
+        var progressbar = $("#" + PROGRESS_BAR_ID + bundleUuid);
+        var progressLabel = $("#" + PROGRESS_LABEL_ID + bundleUuid);
+        progressbar.progressbar({
+          value: 0,
+          max: 100,
+          change: function() {
+            progressLabel.text(progressbar.progressbar("value") + "%");
+          },
+          complete: function() {
+            progressLabel.text("Processing data...");
+          }
+        });
+        var reader = new FileReader();
+        reader.onload = function() {
+          var arrayBuffer = this.result,
+            bytesArray = new Uint8Array(arrayBuffer);
+          $.ajax({
+             url: '/rest/bundles/' + bundleUuid + '/contents/blob/?' + self.getQueryParams(file.name),
+             type: 'PUT',
+             contentType: 'application/octet-stream',
+             data: new Blob([bytesArray]),
+             processData: false,
+             xhr: function() {
+               var xhr = new window.XMLHttpRequest();
+               xhr.upload.addEventListener("progress", function(evt) {
+                 if (evt.lengthComputable) {
+                   var percentComplete = parseInt(evt.loaded / evt.total * 100);
+                   progressbar.progressbar("value", percentComplete);
+                 }
+               }, false);
+               return xhr;
+             },
+             success: function (data, status, jqXHR) {
+               self.clearUploading(fileEntryKey);
+               self.props.refreshWorksheet();
+             },
+             error: function (jqHXR, status, error) {
+               self.clearUploading(fileEntryKey);
+               alert(jqHXR.responseText);
+             }
+          });
+        }
+        reader.readAsArrayBuffer(file);
       }.bind(this),
       error: function (jqHXR, status, error) {
-        this.clearUploading(fileEntryKey);
         alert(jqHXR.responseText);
       }.bind(this)
     });
@@ -63,7 +107,7 @@ var BundleUploader = React.createClass({
     $(this.refs.fileDialog.getDOMNode()).trigger('click');
   },
   render: function () {
-    var upload_button = (
+    var uploadButton = (
       <Button
         text='Upload'
         type='primary'
@@ -76,7 +120,7 @@ var BundleUploader = React.createClass({
     );
     return (
       <div className='inline-block'>
-        {upload_button}
+        {uploadButton}
         <div id="bundle-upload-form" tabIndex="-1" aria-hidden="true">
           <form name="uploadForm" encType="multipart/form-data" method="post">
             <input id="uploadInput" type="file" ref="fileDialog" name="file" onChange={this.uploadBundle} />
@@ -84,11 +128,13 @@ var BundleUploader = React.createClass({
         </div>
 
         <div id="bundle-upload-progress-bars">
-          {_.mapObject(this.state.uploading, function(file, key) {
-            // TODO: show actual upload progress
+          {_.mapObject(this.state.uploading, function(value, key) {
+            var filename = value[0]
+            var bundleUuid = value[1];
             return (
               <div className="bundle-upload-progress-bar progress-bar progress-bar-striped active" role="progressbar">
-                Uploading {file.name}...
+                Uploading {filename}...
+                <div id={PROGRESS_BAR_ID + bundleUuid} className='progressbar'><div id={PROGRESS_LABEL_ID + bundleUuid} className='progress-label'></div></div>
               </div>
             );
           })}
