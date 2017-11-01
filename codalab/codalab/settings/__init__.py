2e31be1aecd2458c9e8e8d4482cda3b3be7b2b89
Max Wang <maxwang7@gmail.com>
2017-11-01 08:33:59 +0000

    increments version to 0.2.19

--- codalab/codalab/settings/__init__.py
+++ codalab/codalab/settings/__init__.py
@@ -19,7 +19,7 @@ class Base(Settings):
     }
 
     # Keep in sync with codalab-cli
-    CODALAB_VERSION = '0.2.18'
+    CODALAB_VERSION = '0.2.19'
 
     ############################################################
 
