f65e498a72013d06b7c3037e0bdd0532626d432b
Max Wang <maxwang7@gmail.com>
2017-11-10 04:38:04 +0000

    implements new actions for bundle search, also adds code to limit number of search results

--- codalab/apps/web/static/js/worksheet/search/actions.jsx
+++ codalab/apps/web/static/js/worksheet/search/actions.jsx
@@ -3,6 +3,9 @@ import fetch from 'isomorphic-fetch'
 const UPDATE_CURRENT_QUERY = 'UPDATE_CURRENT_QUERY';
 const SEARCH_QUERY = 'SEARCH_QUERY';
 const RECEIVE_QUERY_RESULTS = 'RECEIVE_QUERY_RESULTS';
+const SEARCH_USERS = 'SEARCH_USERS';
+const REQUEST_SEARCH_USERS = 'REQUEST_SEARCH_USERS';
+const RECEIVE_SEARCH_USERS = 'RECEIVE_SEARCH_USERS';
 
 
 function updateCurrentQuery(query) {
@@ -20,12 +23,23 @@ function receiveQueryResults(query, results) {
   };
 }
 
+function convertInputToQueryString(input) {
+  let keywordsQuery;
+  keywordsQuery = input.split();
+  keywordsQuery.push(".limit=5");
+  let keywordsQueryString = keywordsQuery.reduce((accumulated, cur) => {
+    return accumulated + `keywords=${encodeURIComponent(cur)}` + '&';
+  }, '');
+  return keywordsQueryString;
+}
+
 function searchQuery(query) {
   console.log(query);
   return (dispatch) => {
     dispatch(updateCurrentQuery(query));
 
-    return fetch(`/rest/worksheets?keywords=${query}`, {
+    let url = `/rest/worksheets?${convertInputToQueryString(query)}`;
+    return fetch(url, {
       credentials: 'same-origin'
     }).then(
         (response) => response.json(),
@@ -37,12 +51,47 @@ function searchQuery(query) {
   }
 }
 
+function searchUsers(query) {
+  return (dispatch) => {
+    dispatch(requestSearchUsers(query));
+
+    return fetch(`/rest/bundles?${convertInputToQueryString(query)}`, {
+      credentials: 'same-origin'
+    }).then(
+      (response) => response.json(),
+      (error) => console.log("error: ", error)
+    ).then(
+      json => dispatch(receiveSearchUsers(query, json))
+    );
+  };
+}
+
+function requestSearchUsers(query) {
+  return {
+    type: RECEIVE_SEARCH_USERS,
+    query,
+  };
+}
+
+function receiveSearchUsers(query, results) {
+  return {
+    type: RECEIVE_SEARCH_USERS,
+    query,
+    results
+  };
+}
 
 export {
   UPDATE_CURRENT_QUERY,
   SEARCH_QUERY,
   RECEIVE_QUERY_RESULTS,
+  SEARCH_USERS,
+  REQUEST_SEARCH_USERS,
+  RECEIVE_SEARCH_USERS,
   updateCurrentQuery,
   receiveQueryResults,
   searchQuery,
+  searchUsers,
+  requestSearchUsers,
+  receiveSearchUsers,
 };
