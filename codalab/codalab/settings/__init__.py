bd95e337b9cc747567e1bb822cce822aca9117e0
Fabian Chan <fabianc@stanford.edu>
2017-11-13 21:45:34 -0800

    increments version to 0.2.20

--- codalab/codalab/settings/__init__.py
+++ codalab/codalab/settings/__init__.py
@@ -19,7 +19,7 @@ class Base(Settings):
     }
 
     # Keep in sync with codalab-cli
-    CODALAB_VERSION = '0.2.19'
+    CODALAB_VERSION = '0.2.20'
 
     ############################################################
 
