e1124af06e7bcfcf808b1805110cbcdb5b154a4d
Max Wang <maxwang7@gmail.com>
2017-12-04 01:41:44 +0000

    implements user searching with codalab-cli branch max-user-branch

--- codalab/apps/web/static/js/worksheet/search/actions.jsx
+++ codalab/apps/web/static/js/worksheet/search/actions.jsx
@@ -5,6 +5,7 @@ import { PARAMS } from './constants.jsx';
 const UPDATE_CURRENT_QUERY = 'UPDATE_CURRENT_QUERY';
 const RECEIVE_SEARCH_WORKSHEETS = 'RECEIVE_SEARCH_WORKSHEETS';
 const RECEIVE_SEARCH_BUNDLES = 'RECEIVE_SEARCH_BUNDLES';
+const RECEIVE_SEARCH_USERS = 'RECEIVE_SEARCH_USERS';
 
 function updateCurrentQuery(query) {
   return {
@@ -56,6 +57,7 @@ function fetchSearch(query) {
       ).catch(
         error => console.log(error)
       ),
+
       fetch(`/rest/bundles?${keywordQueryString}`, {
         credentials: 'same-origin'
       }).then(
@@ -71,6 +73,22 @@ function fetchSearch(query) {
       ).catch(
         error => console.log(error)
       ),
+
+      fetch(`/rest/users?${keywordQueryString}`, {
+        credentials: 'same-origin'
+      }).then(
+        (response) => {
+          if (response.status >= 400) {
+            throw new Error("Bad response from server");
+          }
+          return response.json();
+        },
+        (error) => console.log("error: ", error)
+      ).then(
+        json => dispatch(receiveSearchUsers(query, json))
+      ).catch(
+        error => console.log(error)
+      ),
     ];
   };
 }
@@ -83,9 +101,18 @@ function receiveSearchBundles(query, results) {
   };
 }
 
+function receiveSearchUsers(query, results) {
+  return {
+    type: RECEIVE_SEARCH_USERS,
+    query,
+    results
+  };
+}
+
 export {
   UPDATE_CURRENT_QUERY,
   RECEIVE_SEARCH_WORKSHEETS,
   RECEIVE_SEARCH_BUNDLES,
+  RECEIVE_SEARCH_USERS,
   fetchSearch,
 };
