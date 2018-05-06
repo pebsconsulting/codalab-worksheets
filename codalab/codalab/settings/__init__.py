340cc601a8a665649b1fdf2d2ceaa63b8a68e14a
Dennis Jeong <wonjeo@gmail.com>
2018-05-06 01:11:10 -0700

    Increment version to 0.2.26

--- codalab/codalab/settings/__init__.py
+++ codalab/codalab/settings/__init__.py
@@ -19,7 +19,7 @@ class Base(Settings):
     }
 
     # Keep in sync with codalab-cli
-    CODALAB_VERSION = '0.2.25'
+    CODALAB_VERSION = '0.2.26'
 
     ############################################################
 
