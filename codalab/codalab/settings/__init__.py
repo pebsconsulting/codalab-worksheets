b7e37ff326cba1fb4fa63bf6139c3d9bd4bfc8ce
Kerem Goksel <bkgoksel@gmail.com>
2018-12-31 16:42:18 -0800

    Bump version to 0.2.40

--- codalab/codalab/settings/__init__.py
+++ codalab/codalab/settings/__init__.py
@@ -19,7 +19,7 @@ class Base(Settings):
     }
 
     # Keep in sync with codalab-cli
-    CODALAB_VERSION = '0.2.39'
+    CODALAB_VERSION = '0.2.40'
 
     ############################################################
 
